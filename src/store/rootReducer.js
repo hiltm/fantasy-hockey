23f8f6351ba2c3bb200d63f13bf4da1ea6ca92e9
hiltm <michael.hilt4@gmail.com>
2023-08-17 23:19:15 -0400

    starting fresh

--- src/store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import NHLDataReducer from './AllData/allData.reducer';
-
-const rootReducer = combineReducers({
-  NHLDataReducer: NHLDataReducer,
-});
-
-export default rootReducer;
